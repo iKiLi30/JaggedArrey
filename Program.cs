a649dcd04ba6f8d33649981ebce574c155183b2a
jolahmilovski@gmail.com <jolahmilovski@gmail.com>
2024-09-09 20:18:53 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace JaggedArrey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[][] jaggedArray;
+
+            //jaggedArray = new int[2][];
+            //jaggedArray[0] = new int[2];
+            //jaggedArray[1] = new int[5];
+            int[][] jaggedArray = { new int[] { 1, 3, 7 }, new int[] { 2, 5, 7 }, new int[] { 4, 6, 8 } };
+
+            foreach (int[] i in jaggedArray)
+            {
+                foreach (int j in i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
